1cd0a8400bfaf1d2e66c4ce691c64bcae7aa08c0
Rick van Modem <rick@fluffymedia.nl>
2023-03-01 17:21:17 +0100

    Added experimental SWAFAN support

---
+++ FanControlThermaltake/FanControllers/SWAFAN.cs
@@ -0,0 +1,18 @@
+﻿using HidSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FanControl.ThermaltakeRiingPlus.FanControllers
+{
+    public class SWAFAN : TTFanController, TTFanControllerInterface
+    {
+        public override string Name => "SWAFAN";
+        public override int PortCount => 3;
+
+        public override int ProductIdStart => 0x232b;
+        public override int ProductIdEnd => 0x233B;
+    }
+}
